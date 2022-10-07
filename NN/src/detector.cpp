4532eb53a0db83e234b0b915502b71cad4a9b782
Valetron <notspammeplz@gmail.com>
2022-10-08 01:36:15 +0300

    Build requirements

---
+++ NN/src/detector.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include <opencv2/opencv.hpp>
+//#include <flatbu
+
+#include "../headers/detector.h"
+
+Detector::Detector()
+{
+}
+
+void const Detector::openModel(fs::path& filePath)
+{
+    auto _path = filePath;
+}
