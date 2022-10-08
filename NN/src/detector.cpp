3515a1fcf681454ed34ef8ad241bd64d2b3a72ac
Valetron <notspammeplz@gmail.com>
2022-10-08 12:53:01 +0300

    Пример для класса нейронки

--- NN/src/detector.cpp
+++ NN/src/detector.cpp
@@ -1,15 +1,10 @@
 #include <iostream>
 
 #include <opencv2/opencv.hpp>
-//#include <flatbu
+#include <tensorflow/lite/model.h>
 
 #include "../headers/detector.h"
 
 Detector::Detector()
 {
-}
-
-void const Detector::openModel(fs::path& filePath)
-{
-    auto _path = filePath;
 }
